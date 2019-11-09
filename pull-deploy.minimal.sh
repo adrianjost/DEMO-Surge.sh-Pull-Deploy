f60e6e3ee2873cf1c7f897e818f575cb90999dc1
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-09 11:25:39 +0100

    use generic image because we need npm

--- pull-deploy.minimal.sh
+++ pull-deploy.minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # deploy to surge.sh
-surge --project ${BUILD_DIR} --domain ${TRAVIS_PULL_REQUEST}.${SURGE_SUBDOMAIN}.surge.sh
+npx surge --project ${BUILD_DIR} --domain ${TRAVIS_PULL_REQUEST}.${SURGE_SUBDOMAIN}.surge.sh
 
 # comment url to pull request
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "{\"body\": \"❤️ I have deployed this pull-request for you: \nhttp://${TRAVIS_PULL_REQUEST}.${SURGE_SUBDOMAIN}.surge.sh\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
